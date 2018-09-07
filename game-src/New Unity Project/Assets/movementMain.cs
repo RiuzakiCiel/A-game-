be21d92ac460e08a978611492fb10f947473d179
Yvar <41924494+Yvar192@users.noreply.github.com>
2018-09-07 16:16:40 +0200

    Added an (basically) empty project
    Yeah this is to test stuff

---
+++ game-src/New Unity Project/Assets/movementMain.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementMain : MonoBehaviour {
+
+    float movementSpeed = 4000;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var y = transform.position.y;
+        var x = transform.position.x;
+
+		if (Input.GetKeyDown(KeyCode.D))
+        {
+            x += movementSpeed;
+            Debug.Log("Why doesn't it move");
+        }
+	}
+}
