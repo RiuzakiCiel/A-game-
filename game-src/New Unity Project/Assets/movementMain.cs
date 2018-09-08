05d520a4239f9cc9922e316005bdb00a43a07679
Yvar <41924494+Yvar192@users.noreply.github.com>
2018-09-08 11:37:37 +0200

    Added jumping + basic movement
    Yeah that. This might not be affective

--- game-src/New Unity Project/Assets/movementMain.cs
+++ game-src/New Unity Project/Assets/movementMain.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class movementMain : MonoBehaviour {
 
-    float movementSpeed = 4000;
+    public float movementSpeed = 6;
+    bool canJump = true; 
+    public Vector2 jumpHight;
 
 	// Use this for initialization
 	void Start () {
         
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        var y = transform.position.y;
-        var x = transform.position.x;
 
-		if (Input.GetKeyDown(KeyCode.D))
-        {
-            x += movementSpeed;
-            Debug.Log("Why doesn't it move");
+		if (Input.GetKey(KeyCode.D)) { //Walk right
+            Vector3 tmpPos = transform.position;
+            tmpPos.x += movementSpeed * Time.deltaTime;
+            transform.position = tmpPos;
         }
-	}
+        if (Input.GetKey(KeyCode.A)) { //Walk left
+            Vector3 tmpPos = transform.position;
+            tmpPos.x -= movementSpeed * Time.deltaTime;
+            transform.position = tmpPos;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && canJump == true) { //Jumping
+            GetComponent<Rigidbody2D>().AddForce(jumpHight * Time.deltaTime * 10, ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D theCollision) { //Check if the player is on the ground
+        if (theCollision.gameObject.tag == "floor") {
+            canJump = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D theCollision) {
+        if (theCollision.gameObject.tag == "floor") {
+            canJump = false;
+        }
+    }
 }
